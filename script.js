43d7f160d56290222064efa89fc6e10ba667d637
Cristopher Cortinez Silva <163360618+Ccortinez02@users.noreply.github.com>
2024-08-12 21:19:50 -0400

    agrego login y zona de contacto

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.getElementById('sidebar-toggle').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('open');
-});
-
-document.getElementById('sidebar-close').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.remove('open');
-});
---
+++ scripts.js
@@ -0,0 +1,10 @@
+function mostrarFormulario(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+function cerrarFormulario() {
+    var formularios = document.querySelectorAll('.overlay');
+    formularios.forEach(function(formulario) {
+        formulario.style.display = 'none';
+    });
+}
