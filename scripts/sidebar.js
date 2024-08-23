3fe5999a0a0c2c2ea0df518693878d5acb1915f5
Cristopher Cortinez Silva <163360618+Ccortinez02@users.noreply.github.com>
2024-08-23 10:04:11 -0400

    cambio estilo del sidebar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,11 +2,11 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var body = document.body;
-    if (sidebar.style.width === '250px') {
+    if (sidebar.style.width === '350px') {
         sidebar.style.width = '0';
         body.classList.remove('active-sidebar');
     } else {
-        sidebar.style.width = '250px';
+        sidebar.style.width = '350px';
         body.classList.add('active-sidebar');
     }
 }
